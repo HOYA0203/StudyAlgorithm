7ae657236a2d4db9573b51b3da49c4fc4b26f2d0
TH <TH@218.38.137.28>
2018-04-18 17:03:18 +0900

    18-04-18 Scanner, print

---
+++ N1000/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * N1000 - A + B
+ * https://www.acmicpc.net/problem/1000
+ */
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num1 = 0;
+		int num2 = 0;
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+
+		System.out.println(num1 + num2);
+	}
+}
